d8d45f6cba603418434900c7066cc1b1109404d9
umejolu <jonas@familjenlundin.eu>
2024-07-28 12:17:05 +0200

    Display and fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
 #define RelayPin 7
 #define PotentionmeterPin A0
 
@@ -7,6 +10,23 @@ int weldTime;
 int weldCounter;
 
 unsigned long lastBeat = 0;
+
+/*
+  U8g2lib Example Overview:
+    Frame Buffer Examples: clearBuffer/sendBuffer. Fast, but may not work with all Arduino boards because of RAM consumption
+    Page Buffer Examples: firstPage/nextPage. Less RAM usage, should work with all Arduino boards.
+    U8x8 Text Only Example: No RAM usage, direct communication with display controller. No graphics, 8x8 Text only.
+    
+  This is a page buffer example.    
+*/
+
+// Please UNCOMMENT one of the contructor lines below
+// U8g2 Contructor List (Picture Loop Page Buffer)
+// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+// Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
+
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
 
 void writeIntIntoEEPROM(int address, int number)
 { 
@@ -36,6 +56,8 @@ void setup() {
 
   delay(100);
   Serial.println("Start");
+
+  u8g2.begin(); 
 }
 
 void weld() {
@@ -49,6 +71,7 @@ void weld() {
   digitalWrite(LED_BUILTIN, LOW);
   
   weldCounter++;
+  
   writeIntIntoEEPROM(2, weldCounter);
 
   int buttonValue = analogRead(A7);
@@ -62,7 +85,27 @@ void printWeldTime(){
   Serial.print("Weld time: ");
   Serial.print(weldTime);
   Serial.print(" ");
-  Serial.print(weldCounter);
+  Serial.println(weldCounter);
+
+  u8g2.firstPage();
+  do {
+    //u8g2.drawHLine(0,0,10);
+    //u8g2.drawHLine(0,31,10);
+    //u8g2.clearBuffer();
+    //u8g2.clearDisplay();
+
+    u8g2.setFont(u8g2_font_7x13_tf); //u8g2_font_ncenB10_tr u8g2_font_6x10_tf
+
+    u8g2.drawStr(0,12,"Weld time:");
+    u8g2.setCursor(90,12);        
+    u8g2.print(weldTime);
+
+    u8g2.drawStr(0,28,"Counter:");
+    u8g2.setCursor(90,28);
+    u8g2.print(weldCounter);
+
+  } while ( u8g2.nextPage() );
+
 }
 
 void loop() {
@@ -72,12 +115,19 @@ void loop() {
 
   if (buttonValue <300) {
     weld();
+    printWeldTime();  
+
+    buttonValue = analogRead(A7);
+    while (buttonValue<1000) {
+      delay(50);
+      buttonValue = analogRead(A7);      
+    }
     delay(500);
   }
 
   if (buttonValue >300 && buttonValue <500) {
     Serial.println("Set Weld time");
-    weldTime = analogRead(PotentionmeterPin);
+    weldTime = analogRead(PotentionmeterPin) / 2;
     printWeldTime();   
     writeIntIntoEEPROM(0, weldTime);
 
